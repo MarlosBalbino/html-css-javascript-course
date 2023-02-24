507ea2a060628091ccab330b745c26bcabee8ceb
Marlos Balbino <mbn2@ic.ufal.br>
2023-02-24 09:13:02 -0300

    -

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,16 @@ myImage.onclick = () => {
         myImage.setAttribute("src", "images/ant-man-3.jpeg");
     }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Por favor, digite o seu nome");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla é legal, ${myName}`;
+  }
+
+  myButton.onclick = () => {
+    setUserName();
+  };
